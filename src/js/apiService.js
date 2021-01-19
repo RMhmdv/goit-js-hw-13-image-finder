316eb591c9f6b4f0551a025b3aadfa99e621db5c
Ravshan Mahmudov <2321772@gmail.com>
2021-01-19 10:47:24 +0200

    финальные правки

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,7 +2,7 @@ import { error } from "@pnotify/core";
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
-const API_KEY = '18993455-a304b75c58fb6c345b4521260';
+const API_KEY = '19930843-76dd70daca9d9fc32ede83e2a';
 const BASE_URL = 'https://pixabay.com/api';
 
 export default class ImagesApiService{
